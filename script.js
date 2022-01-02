b1c34e9133650d9aa9d0ffe82af25a4155a9c2b2
David B Wetzel <david@familycatproductions.com>
2022-01-02 03:39:29 +0000

    Added duration to sequences

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ document.querySelector('button') ?.addEventListener('click', async () => {
   console.log('audio is ready')
 })
 
-phrase = [
+var phrase = [
   [[0, "C4"], ["0:1:2", "e4"], ["0:2", "g4"], ["0:3", "e4"], ["1:0:0", "C4"]],
   [[0, "C4"],["0:0:2", "e4"]],
   []
 ]
+
+var seq12 = [{time: "0:0:0", pitch: "f4", dur: "8n"}, {time: "0:0:2", pitch: "g4", dur: "8n"}, {time: "0:1:0", pitch: "b4", dur: "1n"}, {time: "1:1:0", pitch: "c5", dur: "4n"}];
+
 var bpm = 138; // set the global tempo
 
 //create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
 const synth1 = new Tone.Synth().toDestination();
 const synth2 = new Tone.Synth().toDestination();
+const synth3 = new Tone.Synth().toDestination();
+const synth4 = new Tone.Synth().toDestination();
 
 const part = new Tone.Part(((time, note) => {
   // the notes given as the second element in the array
@@ -79,6 +84,39 @@ document.querySelector("button[name='stop']") ?.addEventListener('click', () => 
   console.log('stopping transport')
 })
 
+document.querySelector("button[name='seq1']") ?.addEventListener('click', () => {
+
+  let t = Tone.Transport.position;
+
+  let times = t.split(':');
+  times[2] = 0; // set to downbeat;
+  times[1] = Number(times[1]) + 1; // move up to the next downbeat;
+  if (times[1] > 3) {
+    times[1] = 0;
+    times[0] = Number(times[0]) + 1;
+  }
+
+  console.log("position: " + Tone.Transport.position);
+  console.log("position (array): " + times);
+  console.log("now: " + Tone.Transport.now());
+  t = times[0] + ":" + times[1] + ":" + times[2];
+  console.log("next sub: " + t);
+  sequence1(t);
+
+});
+
+function sequence1(t) {
+  const part = new Tone.Part(((time, note) => {
+    // the notes given as the second element in the array
+    // will be passed in as the second argument
+    console.log("note: " + Tone.Transport.position);
+    synth1.triggerAttackRelease(note, "8n", time);
+  }),
+    [[0, "C4"], ["0:0:0.3", "e4"], ["0:1:0", "c4"], ["0:1:0.3", "e4"], ["0:2:0", "C4"], ["0:2:0.3", "e4"]]).start(t);
+
+  //part.loop = 1;
+}
+
 document.querySelector("button[name='seq2']") ?.addEventListener('click', () => {
 
   let t = Tone.Transport.position;
@@ -112,7 +150,7 @@ function sequence2(t) {
   //part.loop = 1;
 }
 
-document.querySelector("button[name='seq1']") ?.addEventListener('click', () => {
+document.querySelector("button[name='seq3']") ?.addEventListener('click', () => {
 
   let t = Tone.Transport.position;
 
@@ -129,18 +167,83 @@ document.querySelector("button[name='seq1']") ?.addEventListener('click', () => 
   console.log("now: " + Tone.Transport.now());
   t = times[0] + ":" + times[1] + ":" + times[2];
   console.log("next sub: " + t);
-  sequence1(t);
+  sequence3(t);
 
 });
 
-function sequence1(t) {
+function sequence3(t) {
+  const part = new Tone.Part(((time, note) => {
+    // the notes given as the second element in the array
+    // will be passed in as the second argument
+//    console.log("note: " + Tone.Transport.position);
+    console.log("note: " + note.pitch + " dur: " + note.dur);
+    synth3.triggerAttackRelease(note.pitch, note.dur, time);
+  }),[{time: "0:0:2", pitch: "e4", dur: "8n" }, {time: "0:1:0", pitch: "f4", dur: "8n"}, {time: "0:1:2", pitch: "e4", dur: "8n"}]).start(t);
+  //part.loop = 1;
+}
+
+document.querySelector("button[name='seq4']") ?.addEventListener('click', () => {
+
+  let t = Tone.Transport.position;
+
+  let times = t.split(':');
+  times[2] = 0; // set to downbeat;
+  times[1] = Number(times[1]) + 1; // move up to the next downbeat;
+  if (times[1] > 3) {
+    times[1] = 0;
+    times[0] = Number(times[0]) + 1;
+  }
+
+//  console.log("position: " + Tone.Transport.position);
+//  console.log("position (array): " + times);
+//  console.log("now: " + Tone.Transport.now());
+  t = times[0] + ":" + times[1] + ":" + times[2];
+  console.log("next sub: " + t);
+  sequence4(t);
+
+});
+
+function sequence4(t) {
   const part = new Tone.Part(((time, note) => {
     // the notes given as the second element in the array
     // will be passed in as the second argument
     console.log("note: " + Tone.Transport.position);
-    synth1.triggerAttackRelease(note, "8n", time);
+    synth4.triggerAttackRelease(note, "8n", time);
   }),
-    [[0, "C4"], ["0:0:0.3", "e4"], ["0:1:0", "c4"], ["0:1:0.3", "e4"], ["0:2:0", "C4"], ["0:2:0.3", "e4"]]).start(t);
+    [["0:0:2", "e4"], ["0:1:0", "f4"], ["0:1:2", "g4"]]).start(t);
+
+  //part.loop = 1;
+}
+
+document.querySelector("button[name='seq12']") ?.addEventListener('click', () => {
+
+  let t = Tone.Transport.position;
+
+  let times = t.split(':');
+  times[2] = 0; // set to downbeat;
+  times[1] = Number(times[1]) + 1; // move up to the next downbeat;
+  if (times[1] > 3) {
+    times[1] = 0;
+    times[0] = Number(times[0]) + 1;
+  }
+
+//  console.log("position: " + Tone.Transport.position);
+//  console.log("position (array): " + times);
+//  console.log("now: " + Tone.Transport.now());
+  t = times[0] + ":" + times[1] + ":" + times[2];
+  console.log("next sub: " + t);
+  sequence12(t);
+
+});
+
+function sequence12(t) {
+  const part = new Tone.Part(((time, note) => {
+    // the notes given as the second element in the array
+    // will be passed in as the second argument
+    console.log("note: " + Tone.Transport.position);
+    synth4.triggerAttackRelease(note.pitch, note.dur, time);
+  }),
+    seq12).start(t);
 
   //part.loop = 1;
 }
