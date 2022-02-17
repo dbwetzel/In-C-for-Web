b86cf29d856670851b72687f4a570396b74d744e
David B Wetzel <david@familycatproductions.com>
2022-02-17 19:58:49 +0000

    New title

---
+++ InCphrases.js
@@ -0,0 +1,46 @@
+var InCdurations = ["2n.", "2n", "2n", "2n", "2n", "2m", "2m", "4m", "1m", "8n", "4n.", "1n."];
+
+var InC_phrases = [
+  {  /** #1 */
+    num: 1,
+    duration: "2n.",
+    octave: 0,
+    image: "sco1.png",
+    sequence: [{ time: 0, pitch: "C4", dur: "32n" }, { time: "0:0:0.3", pitch: "e4", dur: "4n" }, { time: "0:1:0", pitch: "c4", dur: "32n" }, { time: "0:1:0.3", pitch: "e4", dur: "4n" }, { time: "0:2:0", pitch: "C4", dur: "32n" }, { time: "0:2:0.3", pitch: "e4", dur: "4n" }]
+  },
+  { /** #2 */
+    num: 2,
+    duration: "2n",
+    octave: 0,
+    image: "sco2.png",
+    sequence : [{ time: 0, pitch: "C4", dur: "32n" }, { time: "0:0:0.3", pitch: "e4", dur: "8n" }, { time: "0:0:2", pitch: "f4", dur: "8n" }, { time: "0:1:0", pitch: "e4", dur: "4n" }]
+  },
+  { /** #3 */
+    num: 3,
+    duration: "2n",
+    octave: 0,
+    image: "sco3.png",
+    sequence : [{ time: "0:0:2", pitch: "e4", dur: "8n" }, { time: "0:1:0", pitch: "f4", dur: "8n" }, { time: "0:1:2", pitch: "e4", dur: "8n" }]
+  }, 
+  { /** #4 */
+    num: 4,
+    duration: "2n",
+    octave: 0,
+    image: "sco4.png",
+    sequence: [{ time: "0:0:2", pitch: "e4", dur: "8n" }, { time: "0:1:0", pitch: "f4", dur: "8n" }, { time: "0:1:2", pitch: "g4", dur: "8n" }]
+  }, 
+  { /** #5 */
+    num: 5,
+    duration: "2n",
+    octave: 0,
+    image: "sco5.png",
+    sequence: [{ time: 0, pitch: "e4", dur: "8n" }, { time: "0:0:2", pitch: "f4", dur: "8n" }, { time: "0:1:0", pitch: "g4", dur: "8n" }]
+  }, 
+  [{ time: 0, pitch: "c5", dur: "2m" }], /** #6 */
+  [{ time: "0:3:2", pitch: "c4", dur: "16n" }, { time: "0:3:3", pitch: "c4", dur: "16n" }, { time: "1:0:0", pitch: "c4", dur: "8n" }], /** #7 */
+  [{ time: 0, pitch: "g4", dur: "1n." }, { time: "1:2:0", pitch: "f4", dur: "2m" }], /** #8 */
+  [{ time: 0, pitch: "b4", dur: "16n" }, { time: "0:0:1", pitch: "g4", dur: "16n" }], /** #9 */
+  [{ time: 0, pitch: "b4", dur: "16n" }, { time: "0:0:1", pitch: "g4", dur: "16n" }], /** #10 */
+  [{ time: 0, pitch: "f4", dur: "16n" }, { time: "0:0:1", pitch: "g4", dur: "16n" }, { time: "0:0:2", pitch: "b4", dur: "16n" }, { time: "0:0:3", pitch: "g4", dur: "16n" }, { time: "0:1:0", pitch: "b4", dur: "16n" }, { time: "0:1:1", pitch: "g4", dur: "16n" }], /** #11 */
+  [{ time: "0:0:0", pitch: "f4", dur: "8n" }, { time: "0:0:2", pitch: "g4", dur: "8n" }, { time: "0:1:0", pitch: "b4", dur: "1n" }, { time: "1:1:0", pitch: "c5", dur: "4n" }] /** #12 */
+];
