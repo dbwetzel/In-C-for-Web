27a641c5510013ecd4c9993d03c17a3bae58ebf7
David B Wetzel <david@familycatproductions.com>
2023-02-13 21:40:38 +0000

    added animated loop button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var path = document.querySelector('svg path');
+console.log("svg path " + path.getTotalLength());
+
 document.querySelector('button')?.addEventListener('click', async () => {
   await Tone.start()
   console.log('audio is ready')
@@ -11,8 +14,24 @@ document.getElementById("about")?.addEventListener('click', () => {
     x.style.display = "none";
   }
 });
+document.getElementById("credits")?.addEventListener('click', () => {
+  let x = document.getElementById('InC-credits');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
 document.getElementById("metronome")?.addEventListener('click', () => {
   let x = document.getElementById('metro');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+document.getElementById("sounds")?.addEventListener('click', () => {
+  let x = document.getElementById('sound-controls');
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -116,6 +135,27 @@ for (let i = 0; i < 53; i++) {
   btn.className = "octButton";
   btn.value = "8vb";
   cell1.appendChild(btn);
+  let loopDiv = document.createElement('div');
+  loopDiv.className = "loopDiv";
+  loopDiv.style = "display: block;";
+  let loopForm = document.createElement('form');
+  let loopLabel = document.createElement('label');
+  loopLabel.for = "loop_" + (i + 1);
+  loopLabel.className = "loopLabel";
+  //loopLabel.innerHTML = "Loop #" + (i+1);
+  let loopInput = document.createElement('input');
+  loopInput.id = "loop_" + (i + 1);
+  loopInput.type = "checkbox";
+  loopInput.className = "loopInput";
+  let loopSpanT = document.getElementById('svgTemplate');
+  let loopSpan = loopSpanT.cloneNode(true);
+  loopSpan.id = "loopSpan_" + (i + 1);
+
+  loopLabel.appendChild(loopInput);
+  loopLabel.appendChild(loopSpan);
+  loopForm.appendChild(loopLabel);
+  loopDiv.appendChild(loopForm);
+  cell1.appendChild(loopDiv);
   // Add score images to cell2:
   let img = document.createElement('img');
   img.src = "/images/Sco" + (i + 1) + ".png";
@@ -142,17 +182,33 @@ td.appendChild(btn);
 
 // Attach actions to buttons
 for (let i = 0; i < 53; i++) {
+  let loop = document.getElementById("loop_" + (i + 1));
+  loop.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("looping #" + (i + 1));
+      if(parts[i]) parts[i].loop = true;
+    } else {
+      console.log("stopping #" + (i + 1));
+      if(parts[i]) parts[i].loop = 1;
+    }
+  });
   document.getElementById("sco" + (i + 1))?.addEventListener('mousedown', () => {
     if (Tone.Transport.state = "started") {
+      if(parts[i]){
+        //dispose of the part that's already playing
+        parts[i].dispose();
+      }
       parts[i] = sequence(i);
       if (parts[i]) parts[i].loop = true;
     } else console.log("start metronome first");
   });
   document.getElementById("sco" + (i + 1))?.addEventListener('mouseup', () => {
-    if (parts[i]) parts[i].loop = 1;
+    if (loop.checked) { }
+    else if (parts[i]) parts[i].loop = 1;
   });
   document.getElementById("sco" + (i + 1))?.addEventListener('mouseleave', () => {
-    if (parts[i]) parts[i].loop = 1;
+    if (loop.checked) { }
+    else if (parts[i]) parts[i].loop = 1;
   });
 
   document.getElementById("up8_" + (i + 1))?.addEventListener('click', () => {
